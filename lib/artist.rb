96c61389a657d28ff4a7145ff8da390a44c5287f
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-01 18:43:09 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,16 +2,18 @@ class Artist
   attr_accessor :name
 
 
-  def add_song_by_name(name)
+  def initialize(name)
     @name = name
     @songs = []
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
   end
 
   def songs
     @songs
   end
 
+  def add_song_by_name(song)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+  end
 end
