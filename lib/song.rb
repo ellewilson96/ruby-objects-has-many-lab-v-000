f9c5e889ab1cd77f7f9d881729ee74df945c2e7e
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-01 19:17:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,5 +4,11 @@ class Song
   def initialize(name)
     @name = name
 
+    def artist_name
+      if self.artist != nil
+        return self.artist.name
+      else
+        nil
+      end
 end
 end
