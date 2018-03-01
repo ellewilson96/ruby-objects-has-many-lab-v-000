540e4fa17d08b2a604d60f8afec4990b191b5682
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-01 18:32:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,9 @@
 class Artist
   attr_accessor :name
 
-  def initialize(name)
-    @name = name
-    songs = []
-  end
+  def add_song_by_name(name, genre)
+      song = Song.new(name, genre)
+      @songs << song
+      song.artist = self
+    end
 end
